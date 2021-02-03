99e07069bacb31cafdcdb1ab8281d70b2718b22f
liuhaixin.zero <liuhaixin.zero@bytedance.com>
2021-02-03 21:29:40 +0800

    root

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,14 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-    ext.kotlin_version = Vers.kotlin_version
     repositories {
         google()
         jcenter()
     }
     dependencies {
-        classpath "com.android.tools.build:gradle:4.1.2"
-        classpath Deps.Kotlin.GRADLE_PLUGIN
-        classpath Deps.Kotlin.NO_ARGS
-        classpath Deps.Kotlin.ALL_OPEN
+        classpath("com.android.tools.build:gradle:4.1.2")
+        classpath(Deps.Kotlin.GRADLE_PLUGIN)
+        classpath(Deps.Kotlin.NO_ARGS)
+        classpath(Deps.Kotlin.ALL_OPEN)
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
@@ -22,6 +21,6 @@ allprojects {
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
