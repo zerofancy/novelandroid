ff5d9694b17d6d4af98f251ab59216aecfcafab6
zerofancy <ntutn.top@gmail.com>
2021-05-08 22:56:47 +0800

    [Bug修复](feature/spi): 移除release todo

--- app/src/main/java/top/ntutn/novelrecommend/utils/CrashReportingTree.kt
+++ app/src/main/java/top/ntutn/novelrecommend/utils/CrashReportingTree.kt
@@ -6,7 +6,7 @@ import timber.log.Timber
 class CrashReportingTree : Timber.Tree() {
     override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
         if (priority >= Log.ERROR && t != null) {
-            TODO("执行crash上报的逻辑")
+            // TODO("执行crash上报的逻辑")
         }
     }
 }
